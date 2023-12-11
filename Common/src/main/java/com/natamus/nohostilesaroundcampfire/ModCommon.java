15afc5404ce42bd50a48db70eb189df8084f7ecf
Rick South <rick@natamus.com>
2023-12-12 00:41:42 +0100

    No Hostiles Around Campfire source code for 1.20.0.

---
+++ Common/src/main/java/com/natamus/nohostilesaroundcampfire/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.nohostilesaroundcampfire;
+
+import com.natamus.nohostilesaroundcampfire.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
