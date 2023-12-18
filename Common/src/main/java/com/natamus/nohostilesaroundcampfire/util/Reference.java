0380e3f000f47f416eafcc05a2f5b610a413968a
Rick South <rick@natamus.com>
2023-12-18 10:16:33 +0100

    (v5.8) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/nohostilesaroundcampfire/util/Reference.java
+++ Common/src/main/java/com/natamus/nohostilesaroundcampfire/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.nohostilesaroundcampfire.util;
 public class Reference {
 	public static final String MOD_ID = "nohostilesaroundcampfire";
 	public static final String NAME = "No Hostiles Around Campfire";
-	public static final String VERSION = "5.7";
+	public static final String VERSION = "5.8";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
