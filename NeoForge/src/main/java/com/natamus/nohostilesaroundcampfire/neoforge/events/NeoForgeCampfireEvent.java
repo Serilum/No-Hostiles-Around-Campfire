1563688c5704ab9b9dfe249398e3b2572e5020d7
Rick South <rick@natamus.com>
2024-06-14 05:20:52 +0200

    No Hostiles Around Campfire source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/nohostilesaroundcampfire/neoforge/events/NeoForgeCampfireEvent.java
@@ -0,0 +1,74 @@
+package com.natamus.nohostilesaroundcampfire.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.nohostilesaroundcampfire.events.CampfireEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.neoforge.event.tick.LevelTickEvent;
+
+@EventBusSubscriber
+public class NeoForgeCampfireEvent {
+	@SubscribeEvent
+	public static void onWorldTick(LevelTickEvent.Post e) {
+		Level level = e.getLevel();
+		if (level.isClientSide) {
+			return;
+		}
+		
+		CampfireEvent.onWorldTick((ServerLevel)level);
+	}
+	
+	@SubscribeEvent
+	public static void onEntityCheckSpawn(MobSpawnEvent.PositionCheck e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		if (!CampfireEvent.onEntityCheckSpawn(e.getEntity(), (ServerLevel)level, null, e.getSpawnType())) {
+			e.setResult(MobSpawnEvent.PositionCheck.Result.FAIL);
+		}
+	}
+	
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public static void onCampfirePlace(BlockEvent.EntityPlaceEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		Entity entity = e.getEntity();
+		if (!(entity instanceof LivingEntity)) {
+			return;
+		}
+
+		if (e.isCanceled()) {
+			return;
+		}
+
+		CampfireEvent.onCampfirePlace(level, e.getPos(), e.getPlacedBlock(), (LivingEntity)entity, null);
+	}
+	
+	@SubscribeEvent
+	public static void onRightClickCampfireBlock(PlayerInteractEvent.RightClickBlock e) {
+		CampfireEvent.onRightClickCampfireBlock(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+	
+	@SubscribeEvent
+	public static void onCampfireBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		CampfireEvent.onCampfireBreak(level, e.getPlayer(), e.getPos(), e.getState(), null, null);
+	}
+}
