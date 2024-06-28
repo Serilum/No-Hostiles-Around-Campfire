82a3572757b20564e199a07dceb8b14ba29188df
Rick South <rick@natamus.com>
2024-06-28 15:01:41 +0200

    (v7.0) The mod has been rewritten to drastically improve performance.

--- Common/src/main/java/com/natamus/nohostilesaroundcampfire/ModCommon.java
+++ Common/src/main/java/com/natamus/nohostilesaroundcampfire/ModCommon.java
@@ -1,6 +1,10 @@
 package com.natamus.nohostilesaroundcampfire;
 
+import com.natamus.collective.data.BlockEntityData;
+import com.natamus.collective.globalcallbacks.CachedBlockEntityCallback;
 import com.natamus.nohostilesaroundcampfire.config.ConfigHandler;
+import com.natamus.nohostilesaroundcampfire.events.CampfireEvent;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 
 public class ModCommon {
 
@@ -10,6 +14,14 @@ public class ModCommon {
 	}
 
 	private static void load() {
-		
+		BlockEntityData.addBlockEntityToCache(BlockEntityType.CAMPFIRE);
+
+		CachedBlockEntityCallback.BLOCK_ENTITY_ADDED.register((level, blockEntity, blockEntityType) -> {
+			CampfireEvent.onCampfireAdded(level, blockEntity, blockEntityType);
+		});
+
+		CachedBlockEntityCallback.BLOCK_ENTITY_REMOVED.register((level, blockEntity, blockEntityType) -> {
+			CampfireEvent.onCampfireRemoved(level, blockEntity, blockEntityType);
+		});
 	}
 }
--- Common/src/main/java/com/natamus/nohostilesaroundcampfire/util/Reference.java
+++ Common/src/main/java/com/natamus/nohostilesaroundcampfire/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.nohostilesaroundcampfire.util;
 public class Reference {
 	public static final String MOD_ID = "nohostilesaroundcampfire";
 	public static final String NAME = "No Hostiles Around Campfire";
-	public static final String VERSION = "5.9";
+	public static final String VERSION = "7.0";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
