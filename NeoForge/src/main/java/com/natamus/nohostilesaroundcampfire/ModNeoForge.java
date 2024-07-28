be662d84d789c64d64176f353dcb55cafdce817b
Rick South <rick@natamus.com>
2024-07-28 07:23:27 +0200

    (v7.1) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/nohostilesaroundcampfire/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/nohostilesaroundcampfire/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.nohostilesaroundcampfire;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.nohostilesaroundcampfire.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.nohostilesaroundcampfire.neoforge.events.NeoForgeCampfireEvent;
 import com.natamus.nohostilesaroundcampfire.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
