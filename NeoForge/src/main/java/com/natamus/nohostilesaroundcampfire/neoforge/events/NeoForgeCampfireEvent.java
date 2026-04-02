681168182c38d2938b2a852b87875759086f8559
Rick South <rick@natamus.com>
2026-04-02 16:03:41 +0200

    No Hostiles Around Campfire source code for 26.1.1.

---
+++ NeoForge/src/main/java/com/natamus/nohostilesaroundcampfire/neoforge/events/NeoForgeCampfireEvent.java
@@ -0,0 +1,22 @@
+package com.natamus.nohostilesaroundcampfire.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.nohostilesaroundcampfire.events.CampfireEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.living.MobSpawnEvent;
+
+public class NeoForgeCampfireEvent {
+	@SubscribeEvent
+	public static void onEntityCheckSpawn(MobSpawnEvent.PositionCheck e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		if (!CampfireEvent.onEntityCheckSpawn(e.getEntity(), (ServerLevel)level, null, e.getSpawnType())) {
+			e.setResult(MobSpawnEvent.PositionCheck.Result.FAIL);
+		}
+	}
+}
